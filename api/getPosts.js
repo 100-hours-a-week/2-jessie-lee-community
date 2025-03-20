b2ad14e94010d3551d6a7f178899b27adbfa0fe9
jiyeeeah <sije3757@snu.ac.kr>
2025-03-20 17:11:49 +0900

    refactor: 게시글 페이지 응답에 맞게 수정
    - 웹 서버의 응답에 맞도록 타입 지정
    - 타입에 따라, 제대로 보여지도록 수정

--- api/getPosts.js
+++ api/getPosts.js
@@ -1,8 +1,29 @@
+/**
+ * @typedef {Object} Post
+ * @property {string} id - 게시글 고유 식별자
+ * @property {string} title - 게시글 제목
+ * @property {string} createdAt - 생성 일시
+ * @property {number} likeCount - 좋아요 수
+ * @property {number} viewCount - 조회 수
+ * @property {number} commentCount - 댓글 수
+ * @property {string} userNickname - 작성자 이름
+ */
+
+/**
+ * @typedef {Object} PostsResponse
+ * @property {Post[]} posts - 게시글 목록
+ */
+
+/**
+ * 모든 게시글을 가져오기
+ * @returns {Promise<Post[]>}
+ * @throws {Error} 실패한 경우
+ */
 export async function getPosts() {
   try {
-    const response = await fetch("../../data/posts.json");
+    const response = await fetch("http://localhost:8080/posts");
     const data = await response.json();
-    return data.posts;
+    return data;
   } catch (error) {
     console.error("게시글을 불러오는 중 오류가 발생했습니다:", error);
   }
--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -1,21 +1,27 @@
 import { formatDate } from "../../utils/formatDate.js";
 import { getPosts } from "../../api/getPosts.js";
 
+/**
+ * Post 타입을 import합니다.
+ * @typedef {import('../../api/getPosts.js').Post} Post
+ */
+
 document.addEventListener("DOMContentLoaded", () => {
   displayPosts();
 });
 
 async function displayPosts() {
+  /** @type {Post[]} */
   const posts = await getPosts();
   const postsContainer = document.getElementById("posts-container");
 
-  posts.forEach((post) => {
+  posts.forEach((/** @param {Post} post */ post) => {
     const postElement = createPostElement(post);
     postsContainer.appendChild(postElement);
   });
 }
 
-function createPostElement(post) {
+function createPostElement(/** @param {Post} post */ post) {
   const postElement = document.createElement("a");
   postElement.className = "post";
   postElement.href = `../postdetail/post-detail.html?id=${post.id}`;
@@ -23,13 +29,13 @@ function createPostElement(post) {
   postElement.innerHTML = `
         <div class="post-title">${post.title}</div>
         <div class="post-info">
-            <span>좋아요 ${post.status.likes}</span>
-            <span>댓글 ${post.status.comments}</span>
-            <span>조회수 ${post.status.views}</span>
-            <span class="date">${formatDate(post.date)}</span>
+            <span>좋아요 ${post.likeCount}</span>
+            <span>댓글 ${post.commentCount}</span>
+            <span>조회수 ${post.viewCount}</span>
+            <span class="date">${formatDate(post.createdAt)}</span>
         </div>
         <div class="horizontal-divider"></div>
-        <div class="author-text">${post.author}</div>
+        <div class="author-text">${post.userNickname}</div>
     `;
 
   return postElement;
